96e030ce2397542431ec4076632fb026da9e3cef
kumokumo1653 <fjsda526hdbs@gmail.com>
2020-10-19 08:05:44 +0900

    split page

--- main.rb
+++ main.rb
@@ -5,12 +5,46 @@ set :environment, :production
 NAME_MAX = 15
 ID_MAX = 4
 TEXT_MAX = 100
+PAGE_MAX = 5
 $msg = Msg.new
+
+#投稿件数から最大ページを求める
+def maxPage(len)
+    return ((len + PAGE_MAX - 1) / PAGE_MAX)
+end
+
 get '/' do
     @comments = $msg.msg 
     @begin = 0
     @end = @comments.length
-    erb :index
+    erb :page
+end
+
+get '/:page' do
+    @comments = $msg.msg 
+    #数字かどうか
+    if (params[:page] =~ /^[0-9]+$/) == 0
+        @page = params[:page].to_i
+    else
+        puts "errornum"
+        redirect '/'
+    end
+
+    #ページの範囲内か
+    @minpage = 1
+    @maxpage = maxPage(@comments.length)
+    puts @maxpage
+    if @page >= @minpage.to_i && @page <= @maxpage.to_i 
+        @begin = (@page - 1) * PAGE_MAX
+        if @page == @maxpage.to_i
+            @end = @comments.length
+        else
+            @end = @page * PAGE_MAX 
+        end
+        erb :page
+    else
+        puts "error"
+    end
 end
 
 post '/add' do
